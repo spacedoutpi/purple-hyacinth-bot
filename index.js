f617ff86424bb042026c52b0ceb4d629268a2054
Pi <60800540+spacedoutpi@users.noreply.github.com>
2020-06-07 18:58:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,101 @@ client.on('ready', () =>{
 });
 
 client.on('message', message =>{
-    let args = message.content.slice((prefix).length).trim().split(' ');
-    let cmd = args.shift().toLowerCase();
+    if (message.author.bot || !message.content.startsWith(prefix)) return;
 
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+    var test = message.content.slice((prefix).length).trim().split(" ");
 
-    try {
-        let commandFile = require(`./commands/${cmd}.js`);
-        commandFile.run(client, message, args);
-    } catch (e) {
-        console.log(e.stack);
+    // figure out how to make quotes command concise
+    if (test.indexOf('lauren') != -1) {
+        let args = 'lauren';
+        try {
+            let commandFile = require(`./quotes/${args}.js`);
+            commandFile.run(client, message, args);
+        }
+        catch (e) {
+            console.log(e.stack);
+        }
+    }
+    else if (test.indexOf('kieran') != -1) {
+        let args = 'kieran';
+        try {
+            let commandFile = require(`./quotes/${args}.js`);
+            commandFile.run(client, message, args);
+        }
+        catch (e) {
+            console.log(e.stack);
+        }
+    }
+    else if (test.indexOf('kym') != -1) {
+        let args = 'kym';
+        try {
+            let commandFile = require(`./quotes/${args}.js`);
+            commandFile.run(client, message, args);
+        }
+        catch (e) {
+            console.log(e.stack);
+        }
+    }
+    else if (test.indexOf('william') != -1) {
+        let args = 'william';
+        try {
+            let commandFile = require(`./quotes/${args}.js`);
+            commandFile.run(client, message, args);
+        }
+        catch (e) {
+            console.log(e.stack);
+        }
+    }
+    else if (test.indexOf('miscchar') != -1) {
+        let args = 'miscchar';
+        try {
+            let commandFile = require(`./quotes/${args}.js`);
+            commandFile.run(client, message, args);
+        }
+        catch (e) {
+            console.log(e.stack);
+        }
+    }
+    else if (test.indexOf('lauki') != -1) {
+        let args = 'lauki';
+        try {
+            let commandFile = require(`./quotes/${args}.js`);
+            commandFile.run(client, message, args);
+        }
+        catch (e) {
+            console.log(e.stack);
+        }
+    }
+    else if (test.indexOf('kywi') != -1) {
+        let args = 'kywi';
+        try {
+            let commandFile = require(`./quotes/${args}.js`);
+            commandFile.run(client, message, args);
+        }
+        catch (e) {
+            console.log(e.stack);
+        }
+    }
+    else if (test.indexOf('miscinter') != -1) {
+        let args = 'miscinter';
+        try {
+            let commandFile = require(`./quotes/${args}.js`);
+            commandFile.run(client, message, args);
+        }
+        catch (e) {
+            console.log(e.stack);
+        }
+    }
+    else {
+        let args = test;
+        let cmd = args.shift().toLowerCase();
+        try {
+            let commandFile = require(`./commands/${cmd}.js`);
+            commandFile.run(client, message, args);
+        }
+        catch (e) {
+            console.log(e.stack);
+        }
     }
 });
 
